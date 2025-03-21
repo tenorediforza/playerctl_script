f699c1eefd9e4bc382e9be87c23842ae23f84783
tenorediforza <guillermo.lopez.gutierrez@hotmail.com>
2025-03-21 18:32:54 -0400

    Update firefox_play_pause.sh

--- firefox_play_pause.sh
+++ firefox_play_pause.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 #Small script to keep playerctl working with firefox
-
+#Create the variable to use it accordingly with playerctl
         VAR1=$(playerctl -l | grep firefox)
+#Get the correct output for firefox.instance
         playerctl -l | grep firefox
-#Play or pause firefox.instance_[these numbers change everytime you close firefox]
+#Play or pause firefox.instance variable
         playerctl --player $VAR1 play-pause
